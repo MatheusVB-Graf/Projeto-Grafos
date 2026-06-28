da98d54aa89b94c6fad7c57e37c53042418cc1e5
MatheusVB-Graf <matheusvbl2025@outlook.com>
2026-06-28 15:04:01 -0300

    Organizei o tratamento dos dados

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from datetime import datetime
 DIR_ATUAL = Path(__file__).resolve().parent
 DIR_DADOS = DIR_ATUAL / "dados"
 DIR_RESULT = "resultados"
-LIMIAR = 0.2
+LIMIAR = 0.5
 
 if __name__ == "__main__":
    LISTA_FIIS, df = carregar_dados(DIR_DADOS)
@@ -19,19 +19,33 @@ if __name__ == "__main__":
 
 
    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-   dir_execu = os.path.join(DIR_RESULT, f"execucao_{timestamp}")
+   dir_execu = os.path.join(DIR_RESULT, f"execucao_{timestamp}_LIMIAR_{LIMIAR}")
 
    for periodo, dado_anual in df.groupby(pd.Grouper(freq='YE')):
       ano = periodo.strftime('%Y')
 
+      #pegar apenas FIIs que não possuem 20% dos dados anuais faltantes
+      limite_pct_ano = 0.20
+      faltantes_ano = dado_anual.isna().sum() / len(dado_anual)
+
+      fiis_bons_ano= faltantes_ano[faltantes_ano <= limite_pct_ano].index
+      fiis_removidos_ano = faltantes_ano[faltantes_ano > limite_pct_ano].index
+
+      if len(fiis_removidos_ano) > 0:
+         print(f"Ano {ano} — FIIs removidos por dados incompletos: {fiis_removidos_ano.tolist()}")
+
+      dado_anual = dado_anual[ fiis_bons_ano]
       retornos_ano = dado_anual.pct_change(fill_method=None).dropna()
+
 
       if retornos_ano.empty:
          continue
+
       fiis_grafo = Grafo()
       fiis_grafo.ini_matrizadj(LISTA_FIIS, retornos_ano.corr(),LIMIAR)
       periodo_analisado.add_grafo(ano,fiis_grafo,LISTA_FIIS[1],LISTA_FIIS[35])
       periodo_analisado.salvar_resultados(ano,dir_execu)
+
 
 
 
--- src/fii_grafos/preprocessamento/yfinance.py
+++ src/fii_grafos/preprocessamento/yfinance.py
@@ -4,21 +4,33 @@ import os
 import json
 
 LISTA_FIIS = [
-    # --- FIIs de Papel (Recebíveis / CRIs) ---
-    'MXRF11.SA', 'KNCR11.SA', 'CPTS11.SA', 'IRDM11.SA', 'KNIP11.SA',
-    'VGHF11.SA', 'RECR11.SA', 'HGCR11.SA', 'VRTA11.SA', 'KNSC11.SA',
-    'RBRR11.SA', 'ARRI11.SA', 'CPTR11.SA', 'VGIR11.SA', 'CVBI11.SA',
-    'MCCI11.SA', 'URPR11.SA', 'HCTR11.SA', 'DEVA11.SA', 'HABT11.SA',
-    'BCRI11.SA', 'RBRY11.SA', 'KNHY11.SA', 'OUJP11.SA', 'CACR11.SA',
+    # --- Fundos de Tijolo: Logística ---
+    'HGLG11.SA', 'XPLG11.SA', 'BTLG11.SA', 'VILG11.SA',
+    'BRCO11.SA', 'GGRC11.SA', 'LVBI11.SA', 'RBRL11.SA',
 
-    # --- FIIs de Tijolo e Híbridos (Imóveis Físicos) ---
-    'HGLG11.SA', 'BTLG11.SA', 'XPLG11.SA', 'VILG11.SA', 'LVBI11.SA',
-    'BRCO11.SA', 'GGRC11.SA', 'RBRL11.SA', 'ALZR11.SA', 'HGRE11.SA',
-    'BRCR11.SA', 'PVBI11.SA', 'JSRE11.SA', 'VINO11.SA', 'XPML11.SA',
-    'VISC11.SA', 'HSML11.SA', 'MALL11.SA', 'HGBS11.SA', 'KNRI11.SA',
-    'TRXF11.SA', 'HGRU11.SA', 'RBVA11.SA', 'MAXR11.SA',
+    # --- Fundos de Tijolo: Shoppings ---
+    'XPML11.SA', 'VISC11.SA', 'MALL11.SA', 'HGBS11.SA', 'HSML11.SA',
+
+    # --- Fundos de Tijolo: Lajes corporativas / Escritórios ---
+    'HGRE11.SA', 'BRCR11.SA', 'RCRB11.SA', 'PVBI11.SA',
+    'VINO11.SA', 'BROF11.SA', 'HGPO11.SA',
+
+    # --- Fundos de Tijolo: Agências Bancárias ---
+    'AGCX11.SA', 'BBPO11.SA',
+
+    # --- Fundos de Tijolo: Galpões / Renda Urbana Atípica ---
+    'ALZR11.SA',
+
+    # --- Fundos de Papel (Recebíveis Imobiliários) ---
+    'KNCR11.SA', 'MXRF11.SA', 'KNIP11.SA', 'CPTS11.SA', 'IRDM11.SA',
+    'VGIP11.SA', 'RECR11.SA', 'HGCR11.SA', 'VRTA11.SA', 'KNSC11.SA',
+    'DEVA11.SA', 'MCCI11.SA', 'HCTR11.SA', 'VGIR11.SA', 'OUJP11.SA', 'RBRR11.SA',
+
+
+
+    # --- Fundos de Desenvolvimento ---
+    'RBED11.SA', 'TGAR11.SA',
 ]
-
 ANALISE_INICIAL = "2023"
 ANALISE_FINAL = "2025"
 DATA_INICIAL = f"{ANALISE_INICIAL}-01-01"
@@ -45,20 +57,6 @@ def verifica_listafiis(ids):
         return True
 
     return False
-def tratar_dados(dados):
-    df = dados.dropna(axis=1, how='all')
-
-    faltantes = df.isna().sum().sort_values(ascending=False)
-
-    limite_pct = 0.20
-    total_linhas = len(df)
-    colunas_ruins = faltantes[faltantes > total_linhas * limite_pct].index
-
-    print("Tickers a remover:", colunas_ruins.tolist())
-    df = df.drop(columns=colunas_ruins)
-
-    return df
-
 def carregar_dados(dir_dados):
 
     caminho_arquivo_dados = dir_dados /NOME_DOC
@@ -67,9 +65,8 @@ def carregar_dados(dir_dados):
     if verifica_listafiis(caminho_arquivos_ids) or not os.path.exists(caminho_arquivo_dados):
                  print("Arquivo não encontrado. Baixando do Yahoo Finance...")
                  df_raw = yf.download(LISTA_FIIS, start=DATA_INICIAL, end=DATA_FINAL, threads=False, auto_adjust=False)
-                 df = df_raw['Adj Close']
-                 df_atualizado = tratar_dados(df)
-                 df_atualizado.to_csv(caminho_arquivo_dados)
+                 df = df_raw['Adj Close'].dropna(axis=1, how='all')
+                 df.to_csv(caminho_arquivo_dados)
                  print("Download concluído e dados salvos com sucesso!")
     else:
             print("Arquivo encontrado!")
